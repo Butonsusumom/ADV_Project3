5bac830da99f5e23487fd13f9e85f000cb6a0b9e
Ksusha Tsubulko <43090851+Butonsusumom@users.noreply.github.com>
2021-03-26 14:26:30 +0100

    Revert "Revert "Initial commit""
    This reverts commit 924abf331a2efe2ba4d521112a78e7be4f661263.

---
+++ Assets/ControlAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlAnimation : MonoBehaviour
+{
+    public Animator animator;
+    public string parametrBoolean;
+    bool b = false;
+
+    private void onTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter the trigger");
+        b = !b;
+        animator.SetBool(parametrBoolean, b);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            b = !b;
+            animator.SetBool(parametrBoolean, b);
+        }
+    }
+}
